d5673cf81ddf156b6bdad5d86bf0871094659d14
2h5i <dev.2h5i@gmail.com>
2023-01-24 16:41:09 +0900

    first commit

---
+++ src/main/java/com/test/githubslack/GithubSlackApplication.java
@@ -0,0 +1,13 @@
+package com.test.githubslack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubSlackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubSlackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/test/githubslack/GithubSlackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.githubslack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubSlackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
